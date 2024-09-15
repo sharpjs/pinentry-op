ca30cf9d932f25b42b23c8f76e4d83e336a2fd9c
Jeff Sharp <public@sharpjs.net>
2024-09-15 11:46:47 -0500

    Simplify. No need for log; gpg-agent can do that.

--- src/main.rs
+++ src/main.rs
@@ -2,89 +2,63 @@
 // SPDX-License-Identifier: ISC
 
 // References:
-// https://github.com/gpg/pinentry/blob/master/doc/pinentry.texi
-// https://developer.1password.com/docs/cli/reference/commands/read
-// https://developer.1password.com/docs/cli/reference/management-commands/item
 // https://www.gnupg.org/documentation/manuals/gnupg/Agent-Options.html
 // https://www.gnupg.org/documentation/manuals/assuan/
-// https://github.com/Chronic-Dev/libgpg-error/blob/master/src/err-codes.h.in
-// https://github.com/Chronic-Dev/libgpg-error/blob/master/doc/errorref.txt
+// https://github.com/gpg/pinentry/blob/master/doc/pinentry.texi
+// https://github.com/gpg/libgpg-error/blob/master/src/err-codes.h.in
+// https://github.com/gpg/libgpg-error/blob/master/doc/errorref.txt
+// https://developer.1password.com/docs/cli/reference/commands/read
+// https://developer.1password.com/docs/cli/reference/management-commands/item
 
-use std::{
-    fs, io::{self, BufRead, BufWriter, Write}, process::Command
-};
+use std::io::{self, BufRead, Write};
+use std::process::Command;
 
 fn main() -> io::Result<()> {
     let mut out = io::stdout().lock();
-    let mut log = io::empty(); // BufWriter::new(fs::File::create("pinentry-op.log")?);
 
     writeln!(out, "OK pinentry-op ready")?;
-    writeln!(log, "O: OK pinentry-op ready")?;
 
-    for request in io::stdin().lock().lines() {
-        if !handle(request?.as_str(), &mut out, &mut log)? { break }
+    for req in io::stdin().lock().lines() {
+        if !handle(req?.as_str(), &mut out)? { break }
     }
 
     out.flush()?;
-    log.flush()?;
-
     Ok(())
 }
 
-fn handle<O, L>(req: &str, out: &mut O, log: &mut L) -> io::Result<bool>
-    where O: Write, L: Write
-{
-    writeln!(log, "---")?;
-    writeln!(log, "I: {}", req)?;
-
+fn handle<O: Write>(req: &str, out: &mut O) -> io::Result<bool> {
     let (cmd, _) = match req.split_once(' ') {
         Some(pair) => pair,
         None       => (req, ""),
     };
 
     match cmd {
-        "GETPIN" => handle_getpin (out, log),
-        "BYE"    => handle_bye    (out, log),
-        "FOO"    => handle_unknown(out, log),
-        _        => handle_ignored(out, log),
+        "GETPIN" => handle_getpin (out),
+        "BYE"    => handle_bye    (out),
+        "FOO"    => handle_unknown(out),
+        _        => handle_ignored(out),
     }
 }
 
-fn handle_bye<O, L>(out: &mut O, log: &mut L) -> io::Result<bool>
-where O: Write, L: Write
-{
-    writeln!(out,    "OK closing connection")?;
-    writeln!(log, "O: OK closing connection")?;
+fn handle_bye<O: Write>(out: &mut O) -> io::Result<bool> {
+    writeln!(out, "OK closing connection")?;
     Ok(false)
 }
 
-fn handle_ignored<O, L>(out: &mut O, log: &mut L) -> io::Result<bool>
-where O: Write, L: Write
-{
-    writeln!(out,    "OK ignored command")?;
-    writeln!(log, "O: OK ignored command")?;
+fn handle_ignored<O: Write>(out: &mut O) -> io::Result<bool> {
+    writeln!(out, "OK ignored command")?;
     Ok(true)
 }
 
-fn handle_unknown<O, L>(out: &mut O, log: &mut L) -> io::Result<bool>
-where O: Write, L: Write
-{
-    writeln!(out,    "ERR 275 unsupported command")?;
-    writeln!(log, "O: ERR 275 unsupported command")?;
+fn handle_unknown<O: Write>(out: &mut O) -> io::Result<bool> {
+    writeln!(out, "ERR 275 unsupported command")?;
     Ok(true)
 }
 
-fn handle_getpin<O, L>(out: &mut O, log: &mut L) -> io::Result<bool>
-where O: Write, L: Write
-{
+fn handle_getpin<O: Write>(out: &mut O) -> io::Result<bool> {
     let pin = get_pin()?;
-
-    writeln!(out,    "D {}", pin         )?;
-    writeln!(log, "O: D {}", "(redacted)")?;
-
-    writeln!(out,    "OK")?;
-    writeln!(log, "O: OK")?;
-
+    writeln!(out, "D {}", pin)?;
+    writeln!(out, "OK")?;
     Ok(true)
 }
 
@@ -117,4 +91,3 @@ fn get_pin() -> io::Result<String> {
 fn is_not_ascii_newline(c: char) -> bool {
     !matches!(c, '\r' | '\n')
 }
-
